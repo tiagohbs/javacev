7ca5f866d912e105ea5a932736ed4e5043905227
Tiago Borges <tiagoborges2940@gmail.com>
2022-03-22 17:37:37 -0300

    Initial commit

---
+++ src/com/cev/Caneta.java
@@ -0,0 +1,19 @@
+package com.cev;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void rabiscar(){
+
+    }
+    void tampar(){
+
+    }
+    void destampar(){
+
+    }
+}
---
+++ src/com/cev/Main.java
@@ -0,0 +1,11 @@
+package com.cev;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azull";
+        c1.ponta = 0.5f;
+        c1.tampada = false;
+    }
+}
